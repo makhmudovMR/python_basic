26c36e6b2f02daa1b1d8459afc52dd0dbd0f92aa
Magomed Makhmudov <mmriadovich@gmail.com>
2018-07-20 01:00:11 +0300

    dictionaries

---
+++ dictionaries/dictionaries.py
@@ -0,0 +1,18 @@
+people = {'Magomed': 20, 'Jhon': 49, 'Zamir': 26}
+print(people['Magomed'])
+
+#value
+for v in people.values():
+    print(v)
+
+#keys
+for k in people.keys():
+    print(k)
+
+#keys
+for k in people:
+    print(k)
+
+#value
+for k in people:
+    print(people[k])
