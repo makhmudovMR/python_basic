74c0c22204eeaf678eac19615a322a629d437be7
Magomed Makhmudov <mmriadovich@gmail.com>
2018-07-28 14:15:10 +0300

    sys_module

---
+++ sys_module/sys_module.py
@@ -0,0 +1,16 @@
+'''
+Модуль sys обеспечивает доступ к некоторым переменным и функциям, взаимодействующим с интерпретатором python.
+'''
+
+import sys
+
+# print(sys.copyright)
+
+'''
+sys.stderr.write('Hello world this is kinda error')
+sys.stderr.flush()
+
+sys.stdout.write('this is kinda simple text')
+'''
+
+print(sys.argv)
