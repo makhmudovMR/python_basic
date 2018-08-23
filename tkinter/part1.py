eb3e5b8d9e8d52ac8977dc0181b8e64b2ef18a0e
Magomed Makhmudov <mmriadovich@gmail.com>
2018-08-23 16:02:16 +0300

    gui

--- tkinter/part1.py
+++ tkinter/part1.py
@@ -1,13 +1,25 @@
-from tkinter import *
+from tkinter import Tk, Label, Button
 
+"""
+Create a class which be our app
+"""
+class MyFirstGUI:
+    def __init__(self, master):
+        self.master = master # take the tk object which is an basic component in tk app
+        master.title("A simple GUI") # init title for gui app
 
-class Window(Frame):
+        self.label = Label(master, text="This is our first GUI!")
+        self.label.place(x=0,y=0)
 
-    def __init__(self, master=None):
-        super().__init__(master)
-        self.master = master
+        self.greet_button = Button(master, text="Greet", command=self.greet)
+        self.greet_button.pack()
 
+        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button.pack()
+
+    def greet(self):
+        print("Greetings!")
 
 root = Tk()
-app = Window(root)
+my_gui = MyFirstGUI(root)
 root.mainloop()
